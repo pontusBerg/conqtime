e742c7bbc74add0825275689d6ac1e5adc4bc64d
Pontus Berg <pontusberg95@gmail.com>
2019-11-24 12:45:55 +0100

    fix
    fix

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,31 +14,31 @@
 // document.querySelector('.close-full-popup').addEventListener('click', function(e){
 //   document.querySelector('.card-full-popup').style.display = "none";
 // })
-const x = document.querySelectorAll('.collapsible');
-x.forEach(e => {
- e.addEventListener('click', el => {
+var x = document.querySelectorAll('.collapsible');
+x.forEach(function(e) {
+ e.addEventListener('click', function(e) {
     el.target.nextElementSibling.classList.toggle('block');
  })  
 });
 
 
-document.querySelector('.hamburger-trigger-area').addEventListener('click', e => {
+document.querySelector('.hamburger-trigger-area').addEventListener('click', function(e) {
   document.querySelector('.navbar-content').classList.toggle('flex');
 });
 
 
-document.querySelector('.submit-entry').addEventListener('click', e => {
+document.querySelector('.submit-entry').addEventListener('click', function(e) {
   document.querySelector('.card-journal-entries').style.display = "none";
   document.querySelector('.card-journal-milestones').style.display = "flex";
 })
 
 
 
-document.querySelector('.open-target').addEventListener('click', e => {
+document.querySelector('.open-target').addEventListener('click', function(e) {
   document.querySelector('#popup-goal').classList.toggle('block');
 })
 
-document.querySelector('.close-popup').addEventListener('click', e => {
+document.querySelector('.close-popup').addEventListener('click', function(e) {
   document.querySelector('#popup-goal').classList.toggle('none');
   console.log("hit")
 })
